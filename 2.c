4bc4216c590f1a751e6f03021ab83437eb8108a4
saurabh11m <92452925+saurabh11m@users.noreply.github.com>
2021-10-15 20:49:58 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,24 @@
+//Assignment no.:2(PART-2)
+//CS 201
+//Department of Computer Science and Engineering
+//National Institute of Technology,Silchar
+#include <stdio.h>
+int main() 
+{
+    int a[10], number, i, j;
+    printf("\n Please Enter the Number You want to Convert  :  ");
+    scanf("%d", &number);
+    
+    for(i = 0; number > 0; i++)
+    {
+        a[i] = number % 2;
+        number = number / 2;
+    }
+    
+    printf("\n Binary Number of a Given Number =  ");
+    for(j = i - 1; j >= 0; j--)  {
+        printf("%d", a[j]);
+    }
+    printf("\n");
+    return 0;
+}
